589e4a92274463ff5baf20419fd0372cc0074f8c
Sherwin <sherwin.kwan92@gmail.com>
2021-01-24 16:59:05 -0800

    revert eslint to working state

--- src/components/RoutePoint.jsx
+++ src/components/RoutePoint.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
 
 const RoutePoint = (props) => {
-<<<<<<< HEAD
   const { num, location, lat, long, code } = props.point;
   const pickUp = code.filter(c => c.includes("A"));
   const dropOff = code.filter(c => c.includes("B"));
-=======
-  const { num, location, lat, long, codes } = props.point;
->>>>>>> parent of 87c7419... fix sorting algorithm so points appear in the correct order and with duplicates removed
   return (
     <tr>
       <td>{num}</td>
       <td>{location}</td>
       <td>{lat}</td>
       <td>{long}</td>
-<<<<<<< HEAD
       <td>{pickUp.join(', ')}</td>
       <td>{dropOff.join(', ')}</td>
-=======
-      <td></td>
-      <td></td>
->>>>>>> parent of 87c7419... fix sorting algorithm so points appear in the correct order and with duplicates removed
     </tr>
   );
 };
--- src/helpers/route.js
+++ src/helpers/route.js
@@ -1,32 +1,45 @@
 const findRoute = (movements) => {
-  const points = [];
+  let points = [];
   const southFirst = [];
+  // Convention: "1A" means the starting point of movement 1, "4B" = ending point of movement 4
   movements.forEach(movement => {
     points.push({
+      location: movement.startLocation,
       lat: movement.startLat,
       long: movement.startLong,
-      code: String(movement.id) + "A"
+      code: [String(movement.id) + "A"]
     });
     points.push({
+      location: movement.endLocation,
       lat: movement.endLat,
       long: movement.endLong,
-      code: String(movement.id) + "B"
+      code: [String(movement.id) + "B"]
     });
-    if (movement.endLat > movement.startLat) {
+    if (movement.endLat > movement.startLat || movement.endLat === movement.startLat && movement.endLong > movement.startLong) {
       southFirst.push(String(movement.id) + "B") ;
     }
   });
-  points = points.sort((a, b) => a.lat > b.lat).sort((a, b) => a.long > b.long);
-  const moveToEnd = [];
+  points = points.sort((a, b) => b.long - a.long).sort((a, b) => b.lat - a.lat);
+  let moveToEnd = [];
   for (const code of southFirst) {
-    const indexToMove = points.findIndex(point => point.code === code);
-    moveToEnd.push(points.splice(indexToMove, 1));
+    const indexToMove = points.findIndex(point => point.code[0] === code);
+    moveToEnd.push(points.splice(indexToMove, 1)[0]);
   };
-  moveToEnd = moveToEnd.sort((a, b) => a.lat < b.lat).sort((a, b) => a.long < b.long);;
+  moveToEnd = moveToEnd.sort((a, b) => a.long - b.long).sort((a, b) => a.lat - b.lat);
   points = points.concat(moveToEnd);
+  // If the same point occurs in multiple movements, combine them into one point
+  for (let i = points.length - 1; i > 0; i--) {
+    if (points[i].lat === points[i-1].lat && points[i].long === points[i-1].long) {
+      points[i-1].code.push(points[i].code[0]);
+      points.splice(i, 1);
+    }
+  }
+  // Add a "num" variable to display in the table
   for (let i = 0; i < points.length; i++) {
     points[i] = {...points[i], num: i + 1};
   };
+  console.log('points are: ', points);
+  return points;
 };
 
 export default findRoute;
